da6fc1179f60f4ced34574de0c6637e900996110
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2026-06-25 15:43:49 +0530

    updated types

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,9 +1,18 @@
-declare namespace Express {
-  interface Request {
-    user?: {
-      id: string;
-      email: string;
-      role?: string;
-    };
+import { Request } from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        name: string | null;
+        email: string;
+        password: string;
+        role: "user" | "admin";
+        isVerified: boolean | null;
+        createdAt: Date | null;
+        updatedAt: Date | null;
+      };
+    }
   }
-}   
+}
