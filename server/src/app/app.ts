5b97efb477063e253ca7e611d155724d4bd411f7
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2026-06-29 08:58:44 +0530

    deployment_changes

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,4 +1,4 @@
-import express, {urlencoded} from "express";
+import express, { urlencoded } from "express";
 import type { Express } from "express";
 import { theatreRouter } from "./theatre/theatre.route.js";
 import { authRouter } from "./auth/auth.route.js";
@@ -8,11 +8,18 @@ import { seatRouter } from "./seat/seat.routes.js";
 import { bookingRouter } from "./booking/booking.routes.js";
 import { screenRouter } from "./screen/screen.routes.js";
 import { showRouter } from "./shows/show.routes.js";
+import cors from "cors";
 
 export function createApplication(): Express {
   const app = express();
 
   // Custom CORS middleware to allow local requests from the frontend
+  app.use(
+    cors({
+      origin: "https://ticket-app-rosy.vercel.app/",
+      credentials: true,
+    })
+  );
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
@@ -26,11 +33,11 @@ export function createApplication(): Express {
 
   app.use(express.json());
   app.use(urlencoded({ extended: true, limit: "5mb" }));
-  
+
   app.get("/", (req, res) => {
     return res.json({ message: "Welcome to TicketBooking " });
   });
-  
+
   app.use(authenticationMiddleware());
   app.use("/auth", authRouter);
   app.use("/theatre", theatreRouter);
